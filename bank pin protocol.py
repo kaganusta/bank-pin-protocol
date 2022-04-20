a4a64ecf06852503622051f10ba050ca4c2f1038
kaganusta <104101581+kaganusta@users.noreply.github.com>
2022-04-20 23:15:57 +0300

    Add files via upload

---
+++ bank pin protocol.py
@@ -0,0 +1,42 @@
+
+
+print("Welcome to the world bank" "\n" "Please enter your password")
+
+password= int(11092006)
+
+entering= int(input("Please enter your 8 digit password"))
+
+if int(entering) == int(password):
+    print("You can successfully enter the password correctly.")
+    
+    
+    
+    
+elif int(entering) != int(password):
+    print("The password you entered is incorrect, please try again. Remaining attempts 2")
+
+ent2 = int(input("Please re-enter your password"))
+
+if int(ent2) == int(password):
+    print("You can successfully enter the password correctly.")
+    
+
+elif int(ent2) != int(password):
+    print("The password you entered is incorrect, please try again. Remaining attempts 1")
+
+ent3 = int(input("Please re-enter your password. Last try."))
+
+if int(ent3) == int(password):
+    print("You can successfully enter the password correctly.")
+    
+elif int(ent3) != int(password):
+    print("The password you entered is incorrect" "\n" "Your card is blocked.")
+    import time
+    t = 1.33
+    time.sleep(t)
+elif():
+    ()
+mc = input("Click 5 to find out the customer service number")
+
+if int(mc) == int(mc):
+    print("Customer Service = ""\n""+108504526551")
